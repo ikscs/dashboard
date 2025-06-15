cf8b3ff7814034cac359751a028b887eef49d384
yuriy <y.bugayov@gmail.com>
2025-06-15 21:30:58 +0300

    curses end dashboard

--- backend-/currency_rates/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import CurrencyRate
-
-class CurrencyRateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CurrencyRate
-        fields = ['id', 'currency', 'rate', 'date'] 
